514cd17646ff7a4dfc4352dc7325e38f5505e4db
Amn-siac <trip.aroo10@gmail.com>
2024-06-19 20:55:52 -0400

    day 1 work complete

--- problems/09-unique.js
+++ problems/09-unique.js
@@ -4,7 +4,17 @@ should return a new array containing elements of the input array, without
 duplicates.
 */
 
-// Your code here 
+function unique(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let k = 0; k < arr.length; k++) {
+            if (newArr.includes(arr[i]) !== true) {
+                newArr.push(arr[i])
+            }
+        }
+    }
+    return newArr;
+}
 
 // console.log(unique([1, 1, 2, 3, 3]));         // [1, 2, 3]
 // console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
--- problems/10-intersect.js
+++ problems/10-intersect.js
@@ -3,7 +3,18 @@ Define a function called intersect that takes in two array parameters and
 returns a new array containing the elements common to both arrays.
 */
 
-// Your code here 
+function intersect(arr1, arr2) {
+    let newArr = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        for (let k = 0; k < arr2.length; k++) {
+            if (arr1[i] === arr2[k] && newArr.includes(arr1[i]) !== true) {
+                newArr.push(arr1[i])
+            }
+        }
+    }
+    return newArr;
+}
 
 // console.log(intersect(["a", "b", "c", "d"], ["b", "d", "e"])); //=> [ 'b', 'd' ]
 // console.log(intersect(["a", "b", "c"], ["x", "y", "z"])); //=> []
